c873eec27ad61a7c240cdf64f2b1399b78633914
Kadin Caldwell <kcwell000@gmail.com>
2023-02-03 18:26:07 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1
+
+
+let num3 = 4
+let num4 = 20
+let mod = num3 % num4
+
+
+let max = Math.max(5,10,15,20)
